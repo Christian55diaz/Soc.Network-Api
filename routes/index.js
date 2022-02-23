3ebf20554f56be95dee088658afa700a7c263b9c
Christian55diaz <91170092+Christian55diaz@users.noreply.github.com>
2022-02-22 19:24:25 -0500

    updated to finished route's index

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,14 @@
-//
+//require express for routes
+const router = require('express').Router();
+//import routes from api folder
+const apiRoutes = require('./api');
+
+//router use so api is used for all routed
+router.use('/api', apiRoutes)
+
+//404=error message
+router.use((req, res) => {
+    res.status(404).send('<h1> lol try again 404 ERROR </h1>');
+});
+
+module.exports = router;
