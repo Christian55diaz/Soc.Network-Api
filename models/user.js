3adce84407c351394dad9e33a5e1be0919f655f4
Christian55diaz <91170092+Christian55diaz@users.noreply.github.com>
2022-02-22 19:26:42 -0500

    added new shcema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,18 @@
-//
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            //regex to validate email address
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        }
+    },
