628903bed2147a9ddd81a561590cbf08d092aa94
Christian55diaz <91170092+Christian55diaz@users.noreply.github.com>
2022-02-22 19:25:05 -0500

    updated routes/api index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,10 @@
-//
+//setting requirments + api routes
+const router = require ('express').Router();
+const userRoutes = require('./thoughtsroutes');
+const thoughtsRoutes = require('./userroutes');
+
+//add created routes here
+router.use('/thoughts', thoughtsRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;
