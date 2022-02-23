00ca1fe75f711b64c64e5958fa87a7facffc5e64
Christian55diaz <91170092+Christian55diaz@users.noreply.github.com>
2022-02-22 19:25:38 -0500

    updating thoughts route

--- routes/api/thoughtsroutes.js
+++ routes/api/thoughtsroutes.js
@@ -1,1 +1,12 @@
-//
+const router = require ('express').Router();
+
+//const coming from thought controller to work with insomnia to allow for good routes
+const {
+    getAllThoughts, 
+    getThoughtsById, 
+    createThoughts, 
+    updateThoughts,
+    deleteThoughts
+   
+    //we have to require the thought controller so that the routes link to the controller
+} = require('../../controllers/thoughtscontroller');
