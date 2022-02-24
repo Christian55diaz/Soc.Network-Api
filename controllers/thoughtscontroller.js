b5dcbba78552c2af30bd681646ab9c8840ee9b9d
Christian Diaz <christian55diaz@gmail.com>
2022-02-24 03:54:48 -0500

    updated

--- controllers/thoughtscontroller.js
+++ controllers/thoughtscontroller.js
@@ -19,4 +19,35 @@ const thoughtsController = {
         })
         .catch(err => res.json(err)); 
     },
+    //get all the thoughts
+    getAllThoughts(req,res) {
+        Thoughts.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        // .sort({_id: -1})
+        .then(dbThoughtsData => res.json(dbThoughtsData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    // find one specific thought
+    getThoughtsById({params}, res) {
+        //thought find one
+    Thoughts.findOne({ _id: params.id })
+    .populate({path: 'reactions',select: '-__v'})
+    .select('-__v')
+    .then(dbThoughtsData => {
+        if(!dbThoughtsData) {
+        res.status(404).json({message: 'No thoughts with this particular ID!'});
+        return;
+    }
+    res.json(dbThoughtsData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+},
+
 }
