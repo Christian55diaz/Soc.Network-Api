6fc17ec308afc664bcfb751e9a23da8190da807b
Christian Diaz <christian55diaz@gmail.com>
2022-02-24 03:44:24 -0500

    get all thoughts & create new thought routes added

--- controllers/thoughtscontroller.js
+++ controllers/thoughtscontroller.js
@@ -3,5 +3,20 @@ const {Thoughts, Users} = require('../models');
 
 //make a thoughts controller 
 const thoughtsController = {
-    
+    //new thought created
+    createThoughts({params, body}, res) {
+        //create thoughts in body then the id
+        Thoughts.create(body)
+        .then(({_id}) => {
+            return Users.findOneAndUpdate({ _id: params.userId}, {$push: {thoughts: _id}}, {new: true});
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+                res.status(404).json({message: 'thought not found with this id try again lol'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err)); 
+    },
 }
