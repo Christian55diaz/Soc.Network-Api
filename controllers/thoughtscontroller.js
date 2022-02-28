dc491cdae647f7ecbf30679a7f4f45a4a5ed54ad
Christian Diaz <christian55diaz@gmail.com>
2022-02-27 21:17:47 -0500

    added update and delete routes

--- controllers/thoughtscontroller.js
+++ controllers/thoughtscontroller.js
@@ -39,7 +39,7 @@ const thoughtsController = {
     .select('-__v')
     .then(dbThoughtsData => {
         if(!dbThoughtsData) {
-        res.status(404).json({message: 'No thoughts with this particular ID!'});
+        res.status(404).json({message: 'thought not found with this id try again lol'});
         return;
     }
     res.json(dbThoughtsData)
@@ -49,5 +49,34 @@ const thoughtsController = {
         res.sendStatus(400);
     });
 },
-
+ // here we are deleting a single thought by a specific id
+ deleteThoughts({params}, res) {
+    Thoughts.findOneAndDelete({_id: params.id})
+    .then(dbThoughtsData => {
+        if (!dbThoughtsData) {
+            //if route is executed wrong then a failed message with be shown saying""
+            res.status(404).json({message: 'thought not found with this id try again lol'});
+            return;
+        }
+        res.json(dbThoughtsData);
+        })
+        .catch(err => res.status(400).json(err));
+},
+// updating the thoughts by the specifc id to a new id
+updateThoughts({params, body}, res) {
+    Thoughts.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+    .populate({path: 'reactions', select: '-__v'})
+    .select('-___v')
+    .then(dbThoughtsData => {
+        if (!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts with this particular ID!'});
+            return;
+        }
+            res.json(dbThoughtsData);
+    })
+    .catch(err => res.json(err));
 }
+
+};
+// exporting the module thought controller
+module.exports = thoughtsController;
