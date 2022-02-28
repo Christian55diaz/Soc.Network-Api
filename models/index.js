b7a90a061fe3ba29d167b46aed9c6b6fbcfbe734
Christian Diaz <christian55diaz@gmail.com>
2022-02-28 01:21:00 -0500

    add exports

--- models/index.js
+++ models/index.js
@@ -1,3 +1,6 @@
 // require both the users and thoughts models
 const thoughts = require('./thoughts');
 const users = require('./user');
+
+//exporting both the users and thoughts models
+module.exports = {users, thoughts};
