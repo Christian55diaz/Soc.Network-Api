df91db5ed855962cf9f278a400f22e8732358fcb
Christian Diaz <christian55diaz@gmail.com>
2022-03-10 15:27:23 -0500

    adding the changed model index changes and updated the thoughts model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // require both the users and thoughts models
-const thoughts = require('./Thoughts');
-const users = require('./User');
+// these consts have to match the .js file to their name exactly
+const Thoughts = require('./Thoughts');
+const User = require('./User');
 
 //exporting both the users and thoughts models
-module.exports = {users, thoughts};
+module.exports = {User, Thoughts};
