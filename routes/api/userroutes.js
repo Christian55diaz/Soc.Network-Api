32049842f28661de2ad87c142c4a611306a886c2
Christian Diaz <christian55diaz@gmail.com>
2022-03-10 15:31:20 -0500

    fixed user routes by commenting out the friend routes because it is not finished yet

--- routes/api/userroutes.js
+++ routes/api/userroutes.js
@@ -26,10 +26,11 @@ const {
 
 // routes for the friends params
 // route to add a friend for a user and route to delete a friend from a user
-    router
-    .route('/:userId/friends/:friendId')
-    .post(addFriend)
-    .delete(deleteFriend)
+// add friend is not finished yet
+    // router
+    // .route('/:userId/friends/:friendId')
+    // .post(addFriend)
+    // .delete(deleteFriend)
 
 // export router
     module.exports = router;
